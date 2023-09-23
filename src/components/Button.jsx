c44edbef083aca50bb8cc9098e7f56c26ccf4809
Muhanif Rafi Ahmadi <muhanifrafi@gmail.com>
2023-09-23 16:38:11 +0700

    Penambahan component Textinput dan button

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.button`
+  border: none;
+  padding: 7px 10px;
+  background: green;
+  color:white;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export default styled.input`
+  border: none;
+  padding: 7px 10px;
+  background: white;
+  color: black;
+`;
