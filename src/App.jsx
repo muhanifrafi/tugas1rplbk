c30dafe068637a8fd73dd48aa7de3a0706fa797f
Ahmad Miftachul Hidayat <miftachul.mkb@gmail.com>
2023-09-24 18:59:24 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import Navbar from "./components/Navbar";
+import User from "./components/User";
 
 function App() {
+  const data = [
+    {
+      nama: "A.Miftachul Hidayat",
+      nim: "21120120120002",
+      email: "amiftachulh@students.undip.ac.id",
+    },
+    {
+      nama: "Mirza Ali Abhipraya",
+      nim: "21120120130045",
+      email: "mirzaabhi99@gmail.com",
+    },
+    {
+      nama: "Muhanif Rafi Ahmadi",
+      nim: "21120120130044",
+      email: "muhanifrafi@gmail.com",
+    },
+    {
+      nama: "Zefanya Lois Ligat Sugiarto",
+      nim: "21120120130120",
+      email: "zeflois28@gmail.com",
+    },
+  ];
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <Navbar />
+      <div
+        className="user-wrapper"
+        style={{ display: "flex", gap: "2rem", justifyContent: "center" }}
+      >
+        {data.map(d => (
+          <User key={d.nim} nama={d.nama} nim={d.nim} email={d.email} />
+        ))}
+      </div>
+    </>
   );
 }
 
